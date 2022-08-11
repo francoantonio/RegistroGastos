a97b4514e2984b9c3f5071090444455ab43af919
Antonio Franco <antoniofranco.dev@gmail.com>
2022-08-11 08:28:25 -0300

    se implemento el modulo common y parse-mongo-id.pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SpentModule } from './spent/spent.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
     SpentModule,
     MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    CommonModule,
   ],
   controllers: [],
   providers: [],
